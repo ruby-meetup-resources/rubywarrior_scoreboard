8e7c69b8d3c2c408e0934b8222841ed1dd68eb0e
Patrick Davey <patrick.davey@gmail.com>
2014-11-15 13:06:28 +1300

    Basic commit of the scoreboard app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubywarriorScoreboard::Application.config.secret_token = 'f1175d0a3afc0d123c981d7ccf40037a72d9c1be81d8a79610c23dbcf1ac5ddb880a5c08efbeb7aa29578fcddae60871577be480af5676f42cc4793133ca19fa'
