796157450f885936ae617d79d53b15519ad9a525
Daniel Fone <daniel@fone.net.nz>
2014-11-15 22:34:23 +1300

    Remove secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RubywarriorScoreboard::Application.config.secret_token = 'f1175d0a3afc0d123c981d7ccf40037a72d9c1be81d8a79610c23dbcf1ac5ddb880a5c08efbeb7aa29578fcddae60871577be480af5676f42cc4793133ca19fa'
+RubywarriorScoreboard::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
+
