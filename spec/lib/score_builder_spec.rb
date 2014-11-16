6edf78373c4035d3a8c5f989b41b042f2500dc65
Daniel Fone <daniel@fone.net.nz>
2014-11-16 15:11:07 +1300

    Aspirational specs

---
+++ spec/lib/score_builder_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ScoreBuilder do
+
+  context 'with new warrior' do
+    skip
+  end
+
+  context 'with new level on existing warrior' do
+    skip
+  end
+
+  context 'with existing level on existing warrior' do
+    skip
+  end
+
+end
