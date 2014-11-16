34c8b48bfd1619e7190262bdaed9743f5fd6f13c
Daniel Fone <daniel@fone.net.nz>
2014-11-16 15:01:36 +1300

    Extract method
    Simple method extraction into a module function. The find_or_create had
    to change into a find_or_initialize because we don’t want our module
    function to have side effects.
    
    Now that we’ve extracted, we can (a) test this properly and (b) run the
    logic from the console to input some dummy scores.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,20 +3,7 @@ class ScoresController < ApplicationController
   respond_to :json
 
   def create
-    level_score = params["level_score"]
-    warrior_name = params["warrior_name"]
-    time_bonus = params["time_bonus"]
-    clear_bonus = params["clear_bonus"] || 0
-    total_score = params["total_score"]
-    level_number = params["level_number"]
-
-    warrior = Warrior.find_or_create_by_name(warrior_name)
-    score = warrior.scores.find_or_initialize_by_level_number(level_number)
-    score.time_bonus = time_bonus
-    score.clear_bonus = clear_bonus
-    score.total_score = total_score
-    score.level_score = level_score
-
+    score = ScoreBuilder.build params
     score.save
     respond_with score, location: nil
   end
